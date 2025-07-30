d4953620f8874bc94b6f71e0adbb40b299c4ef2e
yara <yaradeoli@gmail.com>
2025-07-30 11:10:44 +0200

    Creación de los repositorios de árbol y rama

---
+++ src/main/java/es/cic/curso25/proy009/repository/ArbolRepository.java
@@ -0,0 +1,9 @@
+package es.cic.curso25.proy009.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.cic.curso25.proy009.model.Arbol;
+
+public interface ArbolRepository extends JpaRepository<Arbol, Long>{
+
+}
---
+++ src/main/java/es/cic/curso25/proy009/repository/RamaRepository.java
@@ -0,0 +1,9 @@
+package es.cic.curso25.proy009.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.cic.curso25.proy009.model.Rama;
+
+public interface RamaRepository extends JpaRepository<Rama, Long> {
+
+}
