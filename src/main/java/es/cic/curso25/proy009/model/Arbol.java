4fd28d023a6b58a3804a060dde75ec00f6031f78
yara <yaradeoli@gmail.com>
2025-07-30 22:40:26 +0200

    Relación one to many con las entidades árbol y rama

--- src/main/java/es/cic/curso25/proy009/model/Arbol.java
+++ src/main/java/es/cic/curso25/proy009/model/Arbol.java
@@ -1,9 +1,15 @@
 package es.cic.curso25.proy009.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 
@@ -19,11 +25,34 @@ public class Arbol {
     @Version
     private Long version;
 
-    //Atributos Árbol
+    //Relación Uno a Varios
+    //Fetch en relación uno a varios por defecto toma el valor de LAZY
+    @OneToMany(mappedBy = "arbol", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE}, orphanRemoval = true)
+    
+    private List<Rama> ramas = new ArrayList<>();
+
+    //Atributos árbol
+    @Column(name = "nombreComun")
     private String nombreComun;
+    @Column(name = "tipoTronco")
     private String tipoTronco;
+    @Column(name = "altura")
     private double altura;
+    @Column(name = "anchura")
     private double anchura;
+
+    //Métodos para añadir ramas y borrar ramas
+    public void addRama(Rama rama){
+        ramas.add(rama);
+        rama.setArbol(this);
+
+    }
+
+    public void removeRama(Rama rama){
+        ramas.remove(rama);
+        rama.setArbol(null);
+    }
+
 
     //Getters y Setters
     public Long getId() {
@@ -31,6 +60,18 @@ public class Arbol {
     }
     public void setId(Long id) {
         this.id = id;
+    }
+    public Long getVersion() {
+        return version;
+    }
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+    public List<Rama> getRamas() {
+        return ramas;
+    }
+    public void setRamas(List<Rama> ramas) {
+        this.ramas = ramas;
     }
     public String getNombreComun() {
         return nombreComun;
@@ -65,6 +106,7 @@ public class Arbol {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
--- src/main/java/es/cic/curso25/proy009/model/Rama.java
+++ src/main/java/es/cic/curso25/proy009/model/Rama.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
 
@@ -12,7 +14,7 @@ import jakarta.persistence.Version;
 @Table(name = "rama")
 public class Rama {
 
-    //Ramas de árbol con hojas
+    //Ramas de árbol con hojas y algunas con frutos cuando son árboles frutales
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -22,50 +24,83 @@ public class Rama {
     @Version
     private Long version;
 
+    //Relación varios a uno - cada rama pertenece a un único árbol
+    @ManyToOne
+    @JoinColumn(name = "arbol_id") //Foreign key - Crea la foreign key en la tabla rama y la vincula con el árbol correspondiente
 
-    //Atributos de las ramas con hojas
+    private Arbol arbol;
+
+
+    //Atributos de rama
     @Column(name = "colorHoja")
     private String colorHoja;
     @Column(name = "formaHoja")
     private String formaHoja;
-    @Column(name = "tipoCaduca")
-    private boolean tipoCaduca;
-    @Column(name = "tipoPerenne")
-    private boolean tipoPerenne;
+    @Column(name = "frutal")
+    private boolean frutal;
 
     //Getters y Setters
-    public Long getId() {
+        public Long getId() {
         return id;
     }
+
+
     public void setId(Long id) {
         this.id = id;
     }
+
+
+    public Long getVersion() {
+        return version;
+    }
+
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+
+    public Arbol getArbol() {
+        return arbol;
+    }
+
+
+    public void setArbol(Arbol arbol) {
+        this.arbol = arbol;
+    }
+
+
     public String getColorHoja() {
         return colorHoja;
     }
+
+
     public void setColorHoja(String colorHoja) {
         this.colorHoja = colorHoja;
     }
+
+
     public String getFormaHoja() {
         return formaHoja;
     }
+
+
     public void setFormaHoja(String formaHoja) {
         this.formaHoja = formaHoja;
     }
-    public boolean isTipoCaduca() {
-        return tipoCaduca;
-    }
-    public void setTipoCaduca(boolean tipoCaduca) {
-        this.tipoCaduca = tipoCaduca;
-    }
-    public boolean isTipoPerenne() {
-        return tipoPerenne;
-    }
-    public void setTipoPerenne(boolean tipoPerenne) {
-        this.tipoPerenne = tipoPerenne;
+
+
+    public boolean isFrutal() {
+        return frutal;
     }
 
 
+    public void setFrutal(boolean frutal) {
+        this.frutal = frutal;
+    }
+
+
+    
     //hashCode() y equals()
     @Override
     public int hashCode() {
@@ -74,6 +109,11 @@ public class Rama {
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
+
+    
+
+
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -91,13 +131,13 @@ public class Rama {
         return true;
     }
 
+
     //Método toString()
     @Override
     public String toString() {
-        return "Hoja [id=" + id + ", version=" + version + ", colorHoja=" + colorHoja + ", formaHoja=" + formaHoja
-                + ", tipoCaduca=" + tipoCaduca + ", tipoPerenne=" + tipoPerenne + "]";
+        return "Rama [id=" + id + ", version=" + version + ", colorHoja=" + colorHoja + ", formaHoja=" + formaHoja
+                + ", frutal=" + frutal + "]";
     }
-
 
 
 }
